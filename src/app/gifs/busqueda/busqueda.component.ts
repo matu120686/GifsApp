88f19e6bd0cd4b7d314a68dac5747748b49840b2
Matias Olivera <matu120686@gmail.com>
2023-02-12 21:36:24 -0300

    divido en componentes

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+     const valor = this.txtBuscar.nativeElement.value;
+      console.log(valor);
+    
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
